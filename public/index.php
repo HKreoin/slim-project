b7d5aeb43a205cbd0a91cb223e827eb66d411b08
Kreoin <steamuser85@yandex.ru>
2024-10-14 19:48:48 +0300

    add Slim dependencies

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
 // Подключение автозагрузки через composer
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
 
 use Slim\Factory\AppFactory;
 
